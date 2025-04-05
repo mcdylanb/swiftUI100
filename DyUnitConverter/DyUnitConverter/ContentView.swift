715e5cc90b78b179e6e7f98acffef79803d3d79a
Dylan Balagtas <dylansatgalab@gmail.com>
2025-04-06 01:15:39 +0800

    allow dynamic uint conversion km to m and vise versa

--- DyUnitConverter/DyUnitConverter/ContentView.swift
+++ DyUnitConverter/DyUnitConverter/ContentView.swift
@@ -8,25 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var unitToConvert = 0.0
-    @State private var unitConverted = 0.0
+    @State private var amountToConvert = 0.0
+    @State private var unitConverted = "kilometers"
+    @State private var unitToConvert = "meters"
+    
     var body: some View {
-        var kg2Lbs:Double {
-            return unitToConvert * 2.20462
-        }
+        let conversionChoices = ["meters", "kilometers", "feet", "yards", "miles"]
+        
+        var converted:Double {
+            var temp = 0.0
+            temp = amountToConvert
+            if unitToConvert == "meters" && unitConverted == "kilometers"{
+                temp = temp / 1000
+            }else if unitToConvert == "kilometers" && unitConverted == "meters"{
+                temp = temp * 1000
+            }
+            return temp        }
+        
+        
         NavigationStack{
             Form{
-                Section("KG"){
-                    TextField("Amount to Convert", value: $unitToConvert, format: .number)
+                Section("Initial Amount"){
+                    TextField("Amount to Convert", value: $amountToConvert, format: .number)
                         .keyboardType(.decimalPad)
+                    Picker("Select a unit", selection: $unitToConvert){
+                        ForEach(conversionChoices, id: \.self){
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
                 }
                 Section("LBS"){
-                    Text(kg2Lbs, format: .number)
+                    Text(converted, format: .number)
+                    Picker("Select a unit", selection: $unitConverted){
+                        ForEach(conversionChoices, id: \.self){
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
                 }
             }
         }
+        
     }
 }
+
 
 #Preview {
     ContentView()

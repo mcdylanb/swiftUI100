5f3041272845c579fca8b8b4273ea6cf75311738
Dylan Balagtas <dylansatgalab@gmail.com>
2025-04-06 00:30:15 +0800

    basic conversion static kg to lbs todo: make the conversion dynamic

--- DyUnitConverter/DyUnitConverter/ContentView.swift
+++ DyUnitConverter/DyUnitConverter/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var unitToConvert = 0.0
+    @State private var unitConverted = 0.0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        var kg2Lbs:Double {
+            return unitToConvert * 2.20462
         }
-        .padding()
+        NavigationStack{
+            Form{
+                Section("KG"){
+                    TextField("Amount to Convert", value: $unitToConvert, format: .number)
+                        .keyboardType(.decimalPad)
+                }
+                Section("LBS"){
+                    Text(kg2Lbs, format: .number)
+                }
+            }
+        }
     }
 }
 
